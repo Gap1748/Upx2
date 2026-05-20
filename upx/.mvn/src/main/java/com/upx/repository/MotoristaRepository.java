59fe07fd0ff4796cf1bf748d7c01f54b75e3b341
Black Star <2chernayazvezda5@gmail.com>
2026-05-20 23:53:51 +0300

    Gonna die

--- upx/.mvn/src/main/java/com/upx/repository/MotoristaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.upx.repository;
-
-import com.upx.model.entity.Motorista;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
-}
